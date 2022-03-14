b99c94ce7e887b2f39bc6a5a6077e64d138f76bf
LabradorJoseAngelo <joseangelolabrador@gmail.com>
2022-03-14 22:26:49 +0800

    My Educational Background added

--- git-python-labrador.py
+++ git-python-labrador.py
@@ -1,3 +1,6 @@
 print("Jose Angelo L. Labrador")
 print("Tanza,Boac,Marinduque")
 print("July 26, 2001")
+print("Marinduque State College")
+print("Bachelor of Science in Information Technology")
+print("Second Year - 2C")
